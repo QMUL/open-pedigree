004f91b0ceeb14fdb6bb1175b9162e866bf00481
shermanlo77 <s.lo@qmul.ac.uk>
2024-01-12 10:09:00 +0000

    Add backward compatibility following 8de6d44
    Read externalID from either patientResource.identifier OR
    patientResource.externalID

--- src/script/GA4GHFHIRConverter.js
+++ src/script/GA4GHFHIRConverter.js
@@ -608,6 +608,12 @@ GA4GHFHIRConverter.extractDataFromPatient = function (patientResource,
       'https://github.com/phenotips/open-pedigree?externalID') {
       properties.externalID = patientResource.identifier.value;
     }
+  // added for backward compatibility
+  } else if (patientResource.externalID) {
+    if (patientResource.externalID.system ===
+      'https://github.com/phenotips/open-pedigree?externalID') {
+      properties.externalID = patientResource.externalID.value;
+    }
   }
 
   const dateTimeSplitter = /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?/;
